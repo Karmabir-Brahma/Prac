a923117bcb7c9d29064c73ddde4c39f282943af0
Karmabir-Brahma <karmabir8@gmail.com>
2023-08-28 00:27:40 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Connect to Ethereum Wallet like MetaMask and Trust Wallet using React</h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
